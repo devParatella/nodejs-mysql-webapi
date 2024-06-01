ae82b6b5ab4472454145e5539297bf0d73120861
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-01 08:39:52 -0300

    Rota teste

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Its's alive",
+  });
+});
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-} );
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server now is running on port ${process.env.PORT}`);
+});
